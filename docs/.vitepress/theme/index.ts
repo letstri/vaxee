aa8ad93f28d49e10ea79da075ef522fff5ec953d
Valerii Strilets <valerii.strilets@gmail.com>
2024-07-06 00:01:13 +0300

    add vercel analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,11 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme-without-fonts";
+import { inject } from "@vercel/analytics";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
+  enhanceApp() {
+    inject();
+  },
 } satisfies Theme;
